9344e5a2d6b5a417817baf58408ca802bfb1b52f
Raíssa <raissajoaquim29@gmail.com>
2025-08-14 22:08:36 -0300

    navegação programaticamente

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
-    return (
-        <>
-            <h1>Home</h1>
-            <p>go to
-                <Link to="/products">the list of products</Link>
-            </p>
-        </>
-    )
-};
+  const navigate = useNavigate();
+
+  function navigatehandler() {
+    navigate('/produts');
+  }
+
+  return (
+    <>
+      <h1>Home</h1>
+      <p>
+        go to
+        <Link to="/products">the list of products</Link>
+      </p>
+      <p>
+        <button onClick={navigatehandler}>Navigate</button>
+      </p>
+    </>
+  );
+}
 
 export default Home;
