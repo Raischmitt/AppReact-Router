67740328aa50b3093beb8d432f301fb5de00ca44
Raíssa <raissajoaquim29@gmail.com>
2025-08-14 21:53:58 -0300

    adicionando o elemento erro

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from "./pages/Roots";
-
+import ErrorPage from "./pages/Error";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     childrem: [
       { path: "/", element: <Home /> },
       { path: "/products", element: <Products /> },
